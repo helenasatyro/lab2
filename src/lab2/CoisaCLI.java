1fd622a3532654f633f422b72b4ce70e68043b6c
Helena Sátyro <mhsatyro@gmail.com>
2023-04-03 21:14:01 -0300

    Finished

--- src/lab2/CoisaCLI.java
+++ src/lab2/CoisaCLI.java
@@ -1,8 +1,7 @@
-package lab2.CLI;
+package lab2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import lab2.*;
 
 /**
  * Classe que implementa uma interface de Linha de Comando
@@ -18,37 +17,60 @@ public class CoisaCLI {
         ArrayList<Disciplina> disciplinas = new ArrayList<>();
         RegistroResumos resumos = new RegistroResumos(0);
 
+        // Inicialização do menu
         CoisaCLI menu = new CoisaCLI(registrosTempo, registroDescanso, disciplinas, resumos);
         menu.abrirMenu();
     }
     /**
-     * Constante ArrayList contendo os objetos para o menu de tempo online.
+     * Constante ArrayList contendo os objetos RegistroTempoOnline para o menu de tempo online.
      */
     private final ArrayList<RegistroTempoOnline> registrosTempoOnline;
+
+    /**
+     * Constante ArrayList contendo os objetos Disciplina para o menu de disciplinas.
+     */
+    private final ArrayList<Disciplina> disciplinas;
+
+    /**
+     * Constante Descanso para controle de informações sobre descanso.
+     */
     private final Descanso registrosDescanso;
+
+    /**
+     * Objeto RegistroResumos para controle de resumos.
+     */
+    private RegistroResumos resumos;
+
     /**
      * Scanner que será usado na interação com o usuário.
      */
     private final Scanner sc = new Scanner(System.in);
-    private final ArrayList<Disciplina> disciplinas;
-    private RegistroResumos resumos;
+
     /**
      * Construtor de menu, aceita como parâmetros arrays de objetos que podem ser usados no programa.
      * Inicializa os ArrayLists e imprime a mensagem de abertura do programa.
      *
-     * @param registrosTempoOnline ArrayList de objetos do tipo RegistroTempoOnline, pré-inicializado
+     * @param registrosTempoOnline ArrayList de objetos do tipo RegistroTempoOnline, pré-inicializado.
+     * @param registrosDescanso objeto Descanso.
+     * @param disciplinas ArrayList de objetos Disciplinas.
+     * @param resumos objeto resumos inicializado com 0 de espaço.
      */
     public CoisaCLI(ArrayList<RegistroTempoOnline> registrosTempoOnline, Descanso registrosDescanso, ArrayList<Disciplina> disciplinas, RegistroResumos resumos ) {
         this.registrosTempoOnline = registrosTempoOnline;
         this.registrosDescanso = registrosDescanso;
         this.disciplinas = disciplinas;
         this.resumos = resumos;
+        // Texto de cabeçalho da CLI
         System.out.println("""
 
                 		      └[ ∵ ]┘\s
                 -- Olá! Sou RoBetinho, seu assistente acadêmico!"""
                 );
     }
+
+    /**
+     * Exibe o menu inicial e aguarda a escolha de uma opção.
+     */
     public void abrirMenu() {
         String textoMenu = """
 
@@ -64,6 +86,9 @@ public class CoisaCLI {
         System.out.print(textoMenu);
         String escolha = sc.nextLine();
 
+// A única camada de proteção dessa cli contra entradas
+// inválidas é encerrar o programa, o que não é uma forma ideal de se tratar erros.
+// Por motivos de simplicidade da implementação, não foi adicionado tratamento de erros.
         switch (escolha) {
             case "1" -> menuDisciplinas();
             case "2" -> menuResumos();
@@ -74,11 +99,10 @@ public class CoisaCLI {
     }
 
     /**
-     *
+     * Exibe o menu para controle de tempo online.
      */
     private void menuTempoOnline() {
         String textoRegistros = "";
-
         if (registrosTempoOnline.size() != 0) {
             for (int i = 0; i < registrosTempoOnline.size(); i++) {
                 textoRegistros += " 	" + (i + 2) + ". " + registrosTempoOnline.get(i) + "\n";
@@ -93,13 +117,11 @@ public class CoisaCLI {
                 + " Digite uma opção (-1 para sair): ";
 
         System.out.print(menuTempo);
-        int escolha = sc.nextInt();
-        sc.nextLine();
+
+        int escolha = Integer.parseInt(sc.nextLine());
+
         switch (escolha) {
-            case 1 -> {
-                registrosTempoOnline.add(novoRegistro());
-                menuTempoOnline();
-            }
+            case 1 -> registrosTempoOnline.add(novoRegistro());
             case 0 -> this.abrirMenu();
             case -1 -> sair();
             default -> {
@@ -111,8 +133,8 @@ public class CoisaCLI {
                                 VER --Verificar se a meta foi cumprida:\s
                                  A:\s"""
                 );
-                String acao = sc.next();
-                switch (acao.toUpperCase()) {
+                String comando = sc.next();
+                switch (comando.toUpperCase()) {
                     case "ADD" -> {
                         registrosTempoOnline.get(escolha - 2).adicionaTempoOnline(sc.nextInt());
                         sc.nextLine();
@@ -126,22 +148,31 @@ public class CoisaCLI {
         }
     }
 
+    /**
+     * Implementa uma interface de usuário para o construtor de RegistroTempoOnline
+     * @return objeto RegistroTempoOnline que será adicionado ao array de registros de tempo.
+     */
     private RegistroTempoOnline novoRegistro() {
         RegistroTempoOnline registro;
+
         System.out.print("\n--Nome da disciplina: ");
         String nome = sc.nextLine();
         System.out.print("--Tempo online esperado (enter para omitir): ");
         String strHoras = sc.nextLine();
-        if (!strHoras.equals("")) {
-            registro = new RegistroTempoOnline(nome, Integer.parseInt(strHoras));
-        } else {
+
+        if (strHoras.equals("")) {
             registro = new RegistroTempoOnline(nome);
+        } else {
+            registro = new RegistroTempoOnline(nome, Integer.parseInt(strHoras));
         }
         return registro;
     }
 
+    /**
+     * Exibe o menu para interação com o objeto Descanso.
+     */
     private void menuDescanso() {
-        String textoDescanso = """
+        System.out.print("""
 
                 -- DESCANSO --
                      C --Consultar status geral
@@ -149,32 +180,36 @@ public class CoisaCLI {
                      S <semanas> --Definir Número de Semanas
                      E <emoji> --Definir emoji
                      0. Voltar ao menu.
-                 Digite um comando (-1 para sair):\s""";
+                 Digite um comando (-1 para sair):\s"""
+        );
 
-        System.out.print(textoDescanso);
         String escolha = sc.next();
 
         switch (escolha.toUpperCase()) {
             case "C" -> {
                 System.out.println("    >>>STATUS: " + registrosDescanso.getStatusGeral());
                 sc.nextLine();
+                menuDescanso();
             }
             case "H" -> {
                 int horas = sc.nextInt();
                 sc.nextLine();
                 registrosDescanso.defineHorasDescanso(horas);
                 System.out.println("    >>>Adicionadas " + horas + " horas.");
+                menuDescanso();
             }
             case "S" -> {
                 int semanas = sc.nextInt();
                 sc.nextLine();
                 registrosDescanso.defineNumeroSemanas(semanas);
                 System.out.println("    >>>Adicionadas " + semanas + " semanas.");
+                menuDescanso();
             }
             case "E" -> {
                 String emoji = sc.nextLine();
                 registrosDescanso.definirEmoji(emoji);
                 System.out.println("    >>>Definido emoji:  " + emoji);
+                menuDescanso();
             }
             case "0" -> {
                 sc.nextLine();
@@ -182,7 +217,12 @@ public class CoisaCLI {
             }
             default -> sair();
         }
-        menuDescanso();
+        // Observação:
+        // A repetição do método menuDescanso pode parecer redundante, mas colocá-la aqui no final pode
+        // gerar um ciclo recursivo se, por exemplo, repetidamente chamarmos abrirMenu() e menuDescanso(), o método
+        // menuDescanso terminaria chamando a si mesmo, mesmo se saíssemos para o menu.
+        // Essa implementação alternativa está presente no método menuResumos,
+        // note que a IDE exibe um aviso sobre essa falha.
     }
 
         private void menuResumos () {
@@ -242,10 +282,16 @@ public class CoisaCLI {
                 case "0" -> this.abrirMenu();
                 case "-1" -> sair();
             }
-            menuResumos();
+            menuResumos();  // implementação recursiva mantida de forma ilustrativa,
+                            // poderia gerar gastos exagerados de memória.
+                            // Na prática, esse programa é pequeno e tem propósitos educativos, então decidi manter
+                            // a implementação para poder comentar sobre.
         }
 
-        private void menuDisciplinas () {
+    /**
+     * Exibe o menu para controle de notas, pesos e tempo dedicado a uma disciplina.
+     */
+    private void menuDisciplinas () {
             String textoDisciplinas = "";
 
             if (disciplinas.size() != 0) {
@@ -253,8 +299,11 @@ public class CoisaCLI {
                     textoDisciplinas += " 	" + (i + 2) + ". " + disciplinas.get(i) + "\n";
                 }
             }
-
-            String menuDisciplinas = "\n"
+            // Ao longo do programa, diversas formas de escrever blocos de texto foram implementadas.
+            // A forma abaixo é mais intuitiva em alguns casos, mas também há o bloco de três aspas duplas
+            // que é consideravelmente mais agradável esteticamente.
+            String menuDisciplinas =
+                    "\n"
                     + "-- DISCIPLINAS --\n"
                     + " 	1. Adicionar Disciplina\n"
                     + textoDisciplinas
@@ -262,8 +311,8 @@ public class CoisaCLI {
                     + " Digite uma opção (-1 para sair): ";
 
             System.out.print(menuDisciplinas);
-            int escolha = sc.nextInt();
-            sc.nextLine();
+
+            int escolha = Integer.parseInt(sc.nextLine());
             switch (escolha) {
                 case 1 -> {
                     disciplinas.add(novaDisciplina());
@@ -271,18 +320,19 @@ public class CoisaCLI {
                 }
                 case 0 -> this.abrirMenu();
                 case -1 -> sair();
+
                 default -> {
                     System.out.println("\n--O que deseja fazer com esta disciplina?");
-                    System.out.print(
+                    System.out.println(
                             """
                                             
                                     NOTA <nota int> <valor double com vírgula> --Adicionar nota:\s
                                     HORAS --Verificar se a meta foi cumprida:\s
                                     APR --Verificar se foi aprovado:\s
-                                        Digite um comando (-1 para sair): \s"""
+                                        Digite um comando (-1 para sair):"""
                     );
-                    String acao = sc.next();
-                    switch (acao.toUpperCase()) {
+                    String comando = sc.next();
+                    switch (comando.toUpperCase()) {
                         case "NOTA" -> {
                             disciplinas.get(escolha - 2).cadastraNota(sc.nextInt(), sc.nextDouble());
                             sc.nextLine();
@@ -296,7 +346,6 @@ public class CoisaCLI {
                         case "APR" -> {
                             System.out.println("    >>>APROVADO?: " + disciplinas.get(escolha - 2).aprovado());
                             sc.nextLine();
-                            menuDescanso();
                         }
                     }
                     menuDisciplinas();
@@ -305,15 +354,20 @@ public class CoisaCLI {
 
         }
 
+    /**
+     * Implementa uma interface de usuário para o construtor de um objeto Disciplina, que será adicionado ao array de disciplinas.
+     * @return Disciplina
+     */
     private Disciplina novaDisciplina() {
-
         Disciplina disciplina;
+
         System.out.print("\n--Nome da disciplina: ");
         String nome = sc.nextLine();
         System.out.print("--Quantidade de notas(enter para omitir): ");
         String strQtdNotas = sc.nextLine();
         System.out.println("--Especificar pesos? (s/n): ");
         String especificarPesos = sc.nextLine();
+
         if(strQtdNotas.equals("")) {
             strQtdNotas = "4";
         }
@@ -332,6 +386,9 @@ public class CoisaCLI {
         return disciplina;
     }
 
+    /**
+     * Exibe uma mensagem de despedida, fecha o scanner e encerra o programa sem erros.
+     */
     private void sair () {
             System.out.println("\n-- Até a próxima!\n\n			Cc--- [┐∵]┘");
             sc.close();
